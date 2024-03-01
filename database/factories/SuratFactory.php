f38764674c64d19c0c379abf2227ba299eca8b86
wirabudhi <bpantaiselatan@gmail.com>
2024-03-01 10:20:05 +0800

    Add DesaSeeder and SuratSeeder to DatabaseSeeder

--- database/factories/SuratFactory.php
+++ database/factories/SuratFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Desa;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -18,12 +19,17 @@ class SuratFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => User::factory(),
+            'user_id' => $this->faker->randomElement(User::pluck('id')->toArray()),
+            'desa_id' => $this->faker->randomElement(Desa::pluck('id')->toArray()),
+            'jenis_surat' => $this->faker->randomElement(['Surat Masuk', 'Surat Keluar']),
+            'pengirim' => $this->faker->name,
             'perihal' => $this->faker->sentence,
             'tanggal_kegiatan' => $this->faker->date(),
             'hari' => $this->faker->dayOfWeek,
-            'jam_kegiatan' => $this->faker->time(),
+            'waktu' => $this->faker->time(),
             'lokasi_kegiatan' => $this->faker->address,
+            'status' => $this->faker->randomElement(['Dikirim', 'Dikonfirmasi']),
+            'file_surat' => $this->faker->file('public/storage/source', 'public/storage/surat', false),
         ];
     }
 }
