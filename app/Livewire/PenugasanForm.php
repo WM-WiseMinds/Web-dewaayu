d81a99f9e33f710205692400bd2985a8e305ccb7
wirabudhi <bpantaiselatan@gmail.com>
2024-03-06 01:03:18 +0800

    Add penugasan button to SuratTable actions

---
+++ app/Livewire/PenugasanForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PenugasanForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.penugasan-form');
+    }
+}
