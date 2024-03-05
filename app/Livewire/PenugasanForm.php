f9556f00a0b8ddc4499fe98818400476c3c384f2
wirabudhi <bpantaiselatan@gmail.com>
2024-03-06 02:41:45 +0800

    Add statusOnly property to PenugasanForm class and update store method

--- app/Livewire/PenugasanForm.php
+++ app/Livewire/PenugasanForm.php
@@ -15,8 +15,9 @@ class PenugasanForm extends ModalComponent
 
     public Penugasan $penugasan;
     public $id, $user_id, $surat_id, $status, $users, $surats;
+    public $statusOnly = false;
 
-    public function mount($rowId = null, $surat_id = null)
+    public function mount($rowId = null, $surat_id = null, $statusOnly = false)
     {
         $this->penugasan = Penugasan::findOrNew($rowId);
         $this->users = User::role('Anggota TAPM')->get();
@@ -24,8 +25,7 @@ class PenugasanForm extends ModalComponent
         $this->user_id = $this->penugasan->user_id;
         $this->surat_id = $rowId ? $this->penugasan->surat_id : $surat_id;
         $this->status = $rowId ? $this->penugasan->status : 'Ditugaskan';
-
-        dump($this->users);
+        $this->statusOnly = $statusOnly;
     }
 
     public function render()
@@ -52,26 +52,33 @@ class PenugasanForm extends ModalComponent
 
     public function store()
     {
-        $validatedData = $this->validate();
-        $validatedData['status'] = $this->status;
+        if ($this->statusOnly) {
+            $this->penugasan = Penugasan::find($this->id);
+            $this->penugasan->status = $this->status;
+            $this->penugasan->save();
 
-        $this->penugasan = Penugasan::updateOrCreate(['id' => $this->id], $validatedData);
-
-        if ($this->penugasan->wasRecentlyCreated) {
-            $surat = Surat::find($this->surat_id);
-            $surat->status = 'Dikonfirmasi';
-            $surat->save();
-
-            $this->success('Penugasan berhasil ditambahkan');
+            $this->success('Status penugasan berhasil diubah');
         } else {
-            $this->success('Penugasan berhasil diubah');
+            $validatedData = $this->validate();
+            $validatedData['status'] = $this->status;
+
+            $this->penugasan = Penugasan::updateOrCreate(['id' => $this->id], $validatedData);
+
+            if ($this->penugasan->wasRecentlyCreated) {
+                $this->success('Penugasan berhasil ditambahkan');
+            } else {
+                $this->success('Penugasan berhasil diubah');
+            }
         }
+
         $this->closeModalWithEvents([
             PenugasanTable::class => 'penugasanUpdated',
         ]);
 
         $this->resetForm();
 
-        redirect()->route('penugasan');
+        if ($this->penugasan->wasRecentlyCreated) {
+            redirect()->route('penugasan');
+        }
     }
 }
--- resources/views/livewire/penugasan-form.blade.php
+++ resources/views/livewire/penugasan-form.blade.php
@@ -2,26 +2,44 @@
     <form>
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="">
-                <div class="mb-4">
-                    <label for="surat_id" class="block text-gray-700 text-sm font-bold mb-2">Surat</label>
-                    <input wire:model="surat_id" type="text" class="form-input w-full" readonly>
-                    @error('surat_id')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
-                </div>
+                @if (!$statusOnly)
+                    <div class="mb-4">
+                        <label for="surat_id" class="block text-gray-700 text-sm font-bold mb-2">Surat</label>
+                        <input wire:model="surat_id" type="text" class="form-input w-full" readonly>
+                        @error('surat_id')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                <div class="mb-4">
-                    <label for="user_id" class="block text-gray-700 text-sm font-bold mb-2">Penanggung Jawab</label>
-                    <select wire:model="user_id" class="form-select w-full">
-                        <option value="">Pilih Penanggung Jawab</option>
-                        @foreach ($users as $user)
-                            <option value="{{ $user->id }}">{{ $user->name }}</option>
-                        @endforeach
-                    </select>
-                    @error('user_id')
-                        <span class="text-red-500">{{ $message }}</span>
-                    @enderror
-                </div>
+                    <div class="mb-4">
+                        <label for="user_id" class="block text-gray-700 text-sm font-bold mb-2">Penanggung
+                            Jawab</label>
+                        <select wire:model="user_id" class="form-select w-full">
+                            <option value="">Pilih Penanggung Jawab</option>
+                            @foreach ($users as $user)
+                                <option value="{{ $user->id }}">{{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('user_id')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                @else
+                    <div class="mb-4">
+                        <label for="status" class="block text-gray-700 text-sm font-bold mb-2">Status</label>
+                        <div class="flex items-center">
+                            <input wire:model="status" type="radio" value="Disetujui" class="form-radio">
+                            <label for="status" class="ml-2">Disetujui</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input wire:model="status" type="radio" value="Ditolak" class="form-radio">
+                            <label for="status" class="ml-2">Ditolak</label>
+                        </div>
+                        @error('status')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                @endif
 
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                     <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
